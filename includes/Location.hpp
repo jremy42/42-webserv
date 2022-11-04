21273b935424e6060b3646c7089a26eebb025c66
Frederic LE BLAY <fle-blay@e2r1p10.clusters.42paris.fr>
2022-11-04 15:34:03 +0100

    Maj Location class

--- includes/Location.hpp
+++ includes/Location.hpp
@@ -16,21 +16,24 @@
 # include "Response.hpp"
 # include <iterator>
 
-# define DEBUG 1
-
+# define DEBUG_LOCATION 1
 
 class Location
 {
 	public:
 		typedef std::string			string;
 		typedef std::map<string, std::vector<string> > m_s_vs;
-		Location(string rawLocation);
+
+		Location(void);
 		Location(const Location &src);
+		Location(string rawLocation);
 		~Location(void);
 		Location &operator=(const Location &rhs);
+		std::string	normalizeKeyValStr(std::string &keyValStr, const std::string &separatorCharset, const char defaultSeparator);
 
 	private:
-		std::map<std::string, std::vector<std::string> > _createLocationInfoMap(std::string &rawServerConf);
+		void	_createLocationInfoMap(std::string &rawServerConf);
+		std::pair<std::string, std::vector<std::string > >	parseLocationLine(std::string &nextLine);
 		m_s_vs	_locationInfoMap;
 		static std::map<string, int>	_configField;
 		static std::map<string, int> _initConfigField(void);
