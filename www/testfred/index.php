2b35f0826715ad612b0eeed751650cd07a91dd61
Frederic Le Blay <frederic.leblay@gmail.com>
2022-12-05 09:59:07 +0100

    Mini php

---
+++ www/testfred/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p>Start of form</p>
+		<form method="post">
+			<div>
+				<label for="id_user_name">Your name :</label>
+				<input type="text" name="user_name" id="id_user_name">
+			</div>
+			<div>
+				<input type="submit" value="Send">
+			</div>
+		</form>
+		<p>Start of php</p>
+		<?php
+			$var = htmlspecialchars($_POST["user_name"]);
+			echo 'Hello' . $var . '!'
+		?>
+	</body>
+</html>
