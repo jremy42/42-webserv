836395a7e069cd1f42958876c2feb1c73233f088
Frederic Le Blay <frederic.leblay@gmail.com>
2022-12-05 19:03:12 +0100

    wip bash cgi

--- cgi/bash_cgi.sh
+++ cgi/bash_cgi.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo -e "Content-Type: text\r"
-echo -e "\r"
 
 #(bash $1)> /tmp/tmpfile1
 #cat /tmp/tmpfile1
@@ -16,14 +14,14 @@ exec 4>/tmp/tmp_stderr
 exec 5</tmp/tmp_stdin
 
 #Make saves of standard fd
-exec 6>&0
+exec 0<&6
 exec 7>&1
 exec 8>&2
 
 #change standard fd pointing
 exec 1>&3
 exec 2>&4
-exec 0>&5
+exec 5<&0
 
 #execute CGI
 source $1
@@ -36,10 +34,13 @@ exec 2>&8
 #Closes unused fd
 exec 3>&-
 exec 4>&-
-exec 5>&-
+exec 5<&-
 exec 6>&-
 exec 7>&-
 exec 8>&-
+
+echo -e "Content-Type: text\r"
+echo -e "\r"
 
 echo "<!DOCTYPE html>"
 echo "<html>"
