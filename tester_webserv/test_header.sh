bea1675d2608435081724b23b1703086f5e43ab0
Frederic Le Blay <frederic.leblay@gmail.com>
2022-12-07 10:31:54 +0100

    fix test_header.sh

--- tester_webserv/test_header.sh
+++ tester_webserv/test_header.sh
@@ -8,7 +8,7 @@ function test_header ()
 	(( TEST_NUMBER++ ))
 	echo -e "####################################"
 	echo -e "Testing"  "'$1'" "ref : $TEST_NUMBER"
-    cat "$1" | telnet localhost 5002
+	(cat "$1" && sleep 1)| telnet localhost 5002
     RET=$?
     echo -e "RET:$RET"
 	echo -e "####################################"
