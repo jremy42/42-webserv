057ecc459834d1ed65d3237250eb5747b4745955
Frederic Le Blay <frederic.leblay@gmail.com>
2022-12-07 10:25:38 +0100

    maj tester

--- tester_webserv/test_config.sh
+++ tester_webserv/test_config.sh
@@ -2,13 +2,14 @@
 
 TEST_NUMBER="0"
 EXEC="../webserv"
+VALGRIND="valgrind"
 
 function test_config ()
 {
 	(( TEST_NUMBER++ ))
 	echo -e "####################################"
 	echo -e "Testing"  "'$1'" "ref : $TEST_NUMBER"
-    timeout 1 $EXEC "$1"
+    timeout --preserve-status -s SIGINT 1 stdbuf -o0  $VALGRIND $EXEC "$1" >./.test_stdout 2>./.test_stderr
     RET=$?
     echo -e "RET:$RET"
 	echo -e "####################################"
@@ -18,4 +19,15 @@ echo "##### PARSING GLOBAL CONFIG #####"
 for i in `ls bad_conf/**/*`
 do
 	test_config "$i"
+	if test "$RET" -ne 1
+	then
+		echo -e "\e[31mWebserv should have rejected conf : $i\e[0m"
+		echo "STDOUT"
+		test -s ./.test_stdout && cat ./.test_stdout || echo "File empty" 
+		echo "STDERR	"
+		test -s ./.test_stderr && cat ./.test_stderr || echo "File empty" 
+		exit
+	fi
 done
+
+rm -rf ./.test_stdout ./.test_stderr
